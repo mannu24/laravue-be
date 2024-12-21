428b1fe5c26a1614012f54207296d5b02316c281
Manu <mannukumarshah@gmail.com>
2024-12-21 12:13:00 +0530

    Vue setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
-import './bootstrap';
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import ExampleComponent from './components/ExampleComponent.vue';
+import App from './components/App.vue';
+
+const routes = [
+    { path: '/', component: ExampleComponent }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount('#frontend');
---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1 class="pointer">Hello from Vue 3!</h1>
+    </div>
+</template>
