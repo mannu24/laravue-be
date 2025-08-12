8ca59457a4c7a5fabecfd47af6f63722cfc74723
Manu <mannukumarshah@gmail.com>
2025-08-12 16:25:18 +0530

    MInor updates

--- resources/js/components/feed/PostCard.vue
+++ resources/js/components/feed/PostCard.vue
@@ -156,85 +156,27 @@ onMounted(() => {
                                     class="block px-4 py-2 text-sm text-gray-700 transition-all duration-150 ease-in-out rounded-md bg-white hover:bg-gray-200 mb-1">
                                     <i class="fas fa-pencil-alt mr-1"></i>
                                     Edit
-        <div class="flex flex-row border-b-[0.5px] twitter-border-color">
-            <div class="postContent w-full">
-                <header class="flex flex-row w-full items-center gap-2">
-                    <div class="userIcon p-3 pe-1">
-                        <router-link :to="'/' + post.user.username" @click.stop class="w-10 h-10 bg-slate-400 rounded-full">
-                            <img v-if="post.user?.profile_photo" :src="post.user?.profile_photo" alt="User Avatar"
-                                class="w-12 h-12 rounded-full" />
-                            <img v-else src="/assets/front/images/user.png" alt="User Avatar" class="w-12 h-12 rounded-full" />
-                        </router-link>
-                    </div>
-                    <div class="cursor-pointer flex flex-col">
-                        <router-link @click.stop :to="'/' + post.user.username" class='font-bold text-sm'>{{ post.user.name }}</router-link>
-                        <router-link @click.stop :to="'/' + post.user.username" class='text-sm text-gray-500'>@{{ post.user.username }}</router-link>
-                    </div>
-                    <div class="relative inline-block ml-auto" v-if="authStore.isAuthenticated && post.owner">
-                        <p :title="'More Options'" class="icon-hover text-gray-500 hover:bg-primary/20 hover:text-primary hover:bg-opacity-25" @click.stop="showDropdown = !showDropdown">
-                            <LucideEllipsisVertical />
-                        </p>
-                        <transition enter-active-class="transition ease-out duration-100"
-                            enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100"
-                            leave-active-class="transition ease-in duration-75"
-                            leave-from-class="transform opacity-100 scale-100"
-                            leave-to-class="transform opacity-0 scale-95">
-                            <div v-if="showDropdown" v-click-outside="{ closeCondition: () => showDropdown, closeAction: () => showDropdown = false }" class="absolute mt-1 right-2 z-10 w-32 origin-top-right divide-y divide-gray-100 rounded-md focus:outline-hidden"
-                                role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
-                                <div class="" role="none">
-                                    <!-- Active: "bg-gray-100 text-gray-900 outline-hidden", Not Active: "text-gray-700" -->
-                                    <!-- <a href="#" @click="action('duplicate')" class="block px-4 py-2 text-sm text-gray-700 cursor-pointer transition-all duration-150 ease-in-out rounded-md bg-white dark:text-white dark:bg-gray-900 dark:hover:bg-gray-800/80 hover:bg-gray-200">
-                                        <i class="fas fa-files mr-1"></i>
-                                        Duplicate
-                                    </a> -->
-                                    <div @click.stop="edit_post()"
-                                        class="block px-4 py-2 text-sm text-gray-700 cursor-pointer transition-all duration-150 ease-in-out rounded-md shadow dark:shadow-white/10 bg-white dark:text-white dark:bg-gray-900 dark:hover:bg-gray-800/80 hover:bg-gray-200 mb-1">
-                                        <i class="fas fa-pencil-alt mr-1"></i>
-                                        Edit
-                                    </div>
-                                    <AlertDialog>
-                                        <AlertDialogTrigger as-child>
-                                            <div @click.stop
-                                                class="block px-4 py-2 text-sm text-gray-700 cursor-pointer transition-all duration-150 ease-in-out rounded-md shadow dark:shadow-white/10 bg-white dark:text-white dark:bg-gray-900 dark:hover:bg-gray-800/80 hover:bg-gray-200">
-                                                <i class="fas fa-trash mr-1"></i> Delete
-                                            </div>
-                                        </AlertDialogTrigger>
-                                        <AlertDialogContent>
-                                            <AlertDialogHeader>
-                                                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-                                                <AlertDialogDescription>
-                                                    This action cannot be undone. This will delete your post.
-                                                </AlertDialogDescription>
-                                            </AlertDialogHeader>
-                                            <AlertDialogFooter>
-                                                <AlertDialogCancel>Cancel</AlertDialogCancel>
-                                                <AlertDialogAction @click.stop="action('delete')">Continue</AlertDialogAction>
-                                            </AlertDialogFooter>
-                                        </AlertDialogContent>
-                                    </AlertDialog>
                                 </div>
-                            </div>
-                        </transition>
-                    </div>
-                </header>
-                <div class='postText space-y-3'>
-                    <div @click.stop="$router.push(post_url)" class="cursor-pointer px-4">
-                        <h3 class='text-lg font-semibold'>{{ post.title }}</h3>
-                        <p class='text-sm' v-html="renderContent(post.content)" />
-                    </div>
-                    <div v-if="post.media_urls && post.media_urls.length" class="px-2 grid gap-2 mb-4">
-                        <img @click="handleOpenGallery(0)" v-if="post.media_urls.length === 1" :src="post.media_urls[0]" alt="Post Media" class="cursor-pointer rounded-lg object-cover w-full h-80" />
-                        <div v-else-if="post.media_urls.length === 2" class="grid grid-cols-2 gap-2">
-                            <img @click="handleOpenGallery(index)" v-for="(media, index) in post.media_urls" v-if="index < 2" :key="media" :src="media" alt="Post Media" class="cursor-pointer rounded-lg object-cover h-40 w-full" />
-                        </div>
-                        <div v-else-if="post.media_urls.length === 3" class="grid grid-cols-2 gap-2">
-                            <img @click="handleOpenGallery(index)" v-for="(media, index) in post.media_urls.slice(0, 2)" :key="media" :src="media" alt="Post Media" class="cursor-pointer rounded-lg object-cover h-40 w-full" />
-                            <img @click="handleOpenGallery(index)" :src="post.media_urls[2]" alt="Post Media" class="cursor-pointer col-span-2 rounded-lg object-cover h-40 w-full" />
-                        </div>
-                        <div v-else class="grid grid-cols-2 gap-2 relative">
-                            <img @click="handleOpenGallery(index)" v-for="(media, index) in post.media_urls.slice(0, 4)" :key="media" :src="media" alt="Post Media" class="cursor-pointer rounded-lg object-cover h-40 w-full" />
-                            <div @click="handleOpenGallery(3)" v-if="post.media_urls.length > 4" class="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center text-white text-xl font-bold rounded-lg cursor-pointer" :style="{ gridArea: '2 / 2 / 3 / 3' }">
-                                +{{ post.media_urls.length - 4 }} more
+                                <AlertDialog>
+                                    <AlertDialogTrigger as-child>
+                                        <div @click.stop
+                                            class="block px-4 py-2 text-sm text-gray-700 transition-all duration-150 ease-in-out rounded-md bg-white hover:bg-gray-200">
+                                            <i class="fas fa-trash mr-1"></i> Delete
+                                        </div>
+                                    </AlertDialogTrigger>
+                                    <AlertDialogContent>
+                                        <AlertDialogHeader>
+                                            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                                            <AlertDialogDescription>
+                                                This action cannot be undone. This will delete your post.
+                                            </AlertDialogDescription>
+                                        </AlertDialogHeader>
+                                        <AlertDialogFooter>
+                                            <AlertDialogCancel>Cancel</AlertDialogCancel>
+                                            <AlertDialogAction @click.stop="action('delete')">Continue</AlertDialogAction>
+                                        </AlertDialogFooter>
+                                    </AlertDialogContent>
+                                </AlertDialog>
                             </div>
                         </div>
                     </transition>
@@ -247,36 +189,39 @@ onMounted(() => {
                     <img v-if="post.media_urls.length === 1" :src="post.media_urls[0]" alt="Post Media"
                         class="cursor-pointer rounded-lg object-cover w-full h-80" @error="errorFallbackImage" />
                     <div v-else-if="post.media_urls.length === 2" class="grid grid-cols-2 gap-2">
-                        <img v-for="(media, index) in post.media_urls" v-if="index < 2" :key="media" :src="media"
-                            alt="Post Media" class="cursor-pointer rounded-lg object-cover h-40 w-full" @error="errorFallbackImage" />
+                        <template v-for="(media, index) in post.media_urls" :key="media">
+                            <img v-if="index < 2" :src="media" alt="Post Media" class="cursor-pointer rounded-lg object-cover h-40 w-full" @error="errorFallbackImage" />
+                        </template>
                     </div>
                     <div v-else-if="post.media_urls.length === 3" class="grid grid-cols-2 gap-2">
-                        <img v-for="(media, index) in post.media_urls.slice(0, 2)" :key="media" :src="media"
-                            alt="Post Media" class="cursor-pointer rounded-lg object-cover h-40 w-full" @error="errorFallbackImage" />
-                        <img :src="post.media_urls[2]" alt="Post Media"
-                            class="cursor-pointer col-span-2 rounded-lg object-cover h-40 w-full" @error="errorFallbackImage" />
+                        <template v-for="(media, index) in post.media_urls.slice(0, 2)" :key="index">
+                            <img :src="media" alt="Post Media" class="cursor-pointer rounded-lg object-cover h-40 w-full" @error="errorFallbackImage" />
+                        </template>
+                        <img :src="post.media_urls[2]" alt="Post Media" class="cursor-pointer col-span-2 rounded-lg object-cover h-40 w-full" @error="errorFallbackImage" />
                     </div>
                     <div v-else class="grid grid-cols-2 gap-2 relative">
-                        <img v-for="(media, index) in post.media_urls.slice(0, 4)" :key="media" :src="media"
-                            alt="Post Media" class="cursor-pointer rounded-lg object-cover h-40 w-full" @error="errorFallbackImage" />
+                        <template v-for="(media, index) in post.media_urls.slice(0, 4)" :key="index">
+                            <img :src="media" alt="Post Media" class="cursor-pointer rounded-lg object-cover h-40 w-full" @error="errorFallbackImage" />
+                        </template>
                         <div v-if="post.media_urls.length > 4"
                             class="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center text-white text-xl font-bold rounded-lg"
                             :style="{ gridArea: '2 / 2 / 3 / 3' }">
                             +{{ post.media_urls.length - 4 }} more
+                        </div>
                     </div>
-                    <CardFooter class="flex items-center justify-between py-2 px-4 pt-0">
-                        <div class="flex items-center space-x-4">
-                            <span :title="post.likes_count+' Likes'" class="text-gray-500 cursor-pointer hover:text-red-400"><i class="fa-heart" :class="post.liked ? 'fas text-red-500':'far'" @click.stop="handleLike"></i>&nbsp;{{ post.likes_count }}</span>
-                            <span :title="post.comments_count+' Comments'" class="text-gray-500 cursor-pointer hover:text-red-400"><i class="far fa-messages" @click.stop="handleComment"></i>&nbsp;{{ post.comments_count }}</span>
-                            <span :title="post.views_count+' Views'" class="text-gray-500 cursor-pointer hover:text-red-400"><i class="far fa-chart-column"></i>&nbsp;{{ post.views_count }}</span>
-                        </div>
-                        <div class="flex items-center space-x-2">
-                            <span :title="'Posted At'" class="text-gray-500 cursor-pointer text-sm">{{ post.posted_at }}</span>
-                            <span class="bg-red-400 w-[5px] h-[5px] rounded"></span>
-                            <span :title="'Share Post'" class="text-gray-500 cursor-pointer hover:text-red-400"><i class="far fa-paper-plane-top" @click.stop="handleShare"></i></span>
-                        </div>
-                    </CardFooter>
                 </div>
+                <CardFooter class="flex items-center justify-between py-2 px-4 pt-0">
+                    <div class="flex items-center space-x-4">
+                        <span :title="post.likes_count+' Likes'" class="text-gray-500 cursor-pointer hover:text-red-400"><i class="fa-heart" :class="post.liked ? 'fas text-red-500':'far'" @click.stop="handleLike"></i>&nbsp;{{ post.likes_count }}</span>
+                        <span :title="post.comments_count+' Comments'" class="text-gray-500 cursor-pointer hover:text-red-400"><i class="far fa-messages" @click.stop="handleComment"></i>&nbsp;{{ post.comments_count }}</span>
+                        <span :title="post.views_count+' Views'" class="text-gray-500 cursor-pointer hover:text-red-400"><i class="far fa-chart-column"></i>&nbsp;{{ post.views_count }}</span>
+                    </div>
+                    <div class="flex items-center space-x-2">
+                        <span :title="'Posted At'" class="text-gray-500 cursor-pointer text-sm">{{ post.posted_at }}</span>
+                        <span class="bg-red-400 w-[5px] h-[5px] rounded"></span>
+                        <span :title="'Share Post'" class="text-gray-500 cursor-pointer hover:text-red-400"><i class="far fa-paper-plane-top" @click.stop="handleShare"></i></span>
+                    </div>
+                </CardFooter>
             </div>
         </div>
         <!-- <Transition name="fade"> -->
--- resources/js/views/Login.vue
+++ resources/js/views/Login.vue
@@ -45,20 +45,20 @@ const handleComplete = (value) => {
 
 const handleOtpRequest = async () => {
     btn.value = 'loading';
-    await axios.post('/api/v1/auth/otp', { email: loginData.value.email }).then(() => {
+    try {
+        await axios.post('/api/v1/auth/otp', { email: loginData.value.email });
         isOtpSent.value = true;
-        btn.value = false
+        btn.value = false;
         errorMessage.value = '';
         nextTick(() => {
-        setTimeout(() => {
-            init_form()
-            init_timer()
-
-        }, 500);
-    }).catch((error) => {
-        btn.value = false
+            setTimeout(() => {
+                init_timer();
+            }, 500);
+        });
+    } catch (error) {
+        btn.value = false;
         errorMessage.value = error.response?.data?.message || 'Failed to send OTP.';
-    });
+    }
 };
 
 const edit_email = () => {
@@ -68,11 +68,12 @@ const edit_email = () => {
 
 const handleOtpVerification = async () => {
     btn.value = 'loading';
-    await axios.post('/api/v1/auth/otp', {
-        email: loginData.value.email,
-        otp: otp.value
-    }).then((response) => {
-        btn.value = false
+    try {
+        const response = await axios.post('/api/v1/auth/otp', {
+            email: loginData.value.email,
+            otp: otp.value
+        });
+        btn.value = false;
         if (response.data.status == 'success') {
             const data = response.data.data;
             const { token, user } = data;
@@ -83,18 +84,18 @@ const handleOtpVerification = async () => {
             } else {
                 router.push('/')
             }
-        } else errorMessage.value = response.data.message;
-    }).catch((error) => {
-        btn.value = false
-        errorMessage.value = error.response?.data?.message || 'Failed to send OTP.';
-    });
+        } else {
+            errorMessage.value = response.data.message;
+        }
+    } catch (error) {
+        btn.value = false;
+        errorMessage.value = error.response?.data?.message || 'Failed to verify OTP.';
+    }
 };
 
 const handleGoogleCallback = (response) => {
     const decoded = jwtDecode(response.credential);
     console.log(decoded);
-
-
     // form.value.name = decoded.name;
     // form.value.email = decoded.email;
     // form.value.profile_id = decoded.jti.substring(0, 8);
@@ -118,64 +119,6 @@ onMounted(() => {
     <div class="min-h-[80vh] flex items-center justify-center">
         <div class="w-full mx-auto py-24">
             <div class="flex justify-center">
-                <form v-if="!isOtpSent" @submit.prevent="handleOtpRequest()" class="max-w-md mx-auto text-center bg-gray-800/80 px-4 sm:px-8 py-10 rounded-xl shadow">
-                    <header class="mb-8">
-                        <h1 class="text-2xl font-bold mb-3 text-white">Sign in to Laravue</h1>
-                        <p class="text-[15px] text-slate-400">We will need your email to send the verification code.</p>
-                    </header>
-                    <div class="flex items-center justify-center gap-3">
-                        <CustomInput type="text" placeholder="Write Email Here..." v-model="loginData.email" />
-                    </div>
-                    <div v-if="errorMessage" class="text-red-500 text-sm">
-                        {{ errorMessage }}
-                    </div>
-                    <div class="max-w-[260px] mx-auto mt-4">
-                        <button type="submit" :class="btn == 'loading' ? 'cursor-wait bg-vue/70' : 'bg-vue/90 hover:bg-vue'"
-                        class="w-full inline-flex transition-all duration-300 justify-center whitespace-nowrap rounded-lg px-3.5 py-2.5 text-sm font-medium text-white shadow-sm shadow-vue/10 focus:outline-none focus:bg-vue focus:ring focus:ring-vue/40 focus-visible:outline-none focus-visible:ring focus-visible:ring-vue/40">Get OTP</button>
-                    </div>
-                </form>
-                <form v-else id="otp-form" @submit.prevent="handleOtpVerification()" class="max-w-md mx-auto text-center bg-gray-800/80 px-4 sm:px-8 py-10 rounded-xl shadow">
-                    <header class="mb-8">
-                        <h1 class="text-2xl font-bold mb-3 text-white">Email Verification</h1>
-                        <p class="text-[15px] text-slate-400">Enter the 6-digit verification code that was sent to <br>
-                            <div class="flex gap-2 items-center justify-center mt-1">
-                                <b>"{{ loginData.email }}"</b>
-                                <i class="fas fa-pencil-alt hover:text-white cursor-pointer" @click="edit_email"></i>
-                            </div>
-                        </p>
-                    </header>
-                    <div class="flex items-center justify-center">
-                        <PinInput
-                            id="pin-input"
-                            v-model="loginData.otp"
-                            placeholder="○"
-                            @complete="handleComplete"
-                        >
-                            <PinInputGroup class="gap-1">
-                                <template v-for="(id, index) in 6" :key="id">
-                                    <PinInputSlot
-                                        class="rounded-md border w-12 h-12 text-center text-2xl font-extrabold text-gray-100 bg-gray-700 border-transparent hover:border-slate-200 focus:bg-gray-500 focus:border-vue/90 focus:ring-2 focus:ring-vue/40"
-                                        :index="index"
-                                    />
-                                    <template v-if="index !== 5">
-                                        <PinInputSeparator />
-                                    </template>
-                                </template>
-                            </PinInputGroup>
-                        </PinInput>
-                    </div>
-                    <div v-if="errorMessage" class="text-laravel/80 text-sm mt-3">
-                        {{ errorMessage }}
-                    </div>
-                    <div class="max-w-[260px] mx-auto mt-4">
-                        <button type="submit" :class="btn == 'loading' ? 'cursor-wait bg-vue/70' : 'bg-vue/90 hover:bg-vue'"
-                        class="w-full inline-flex transition-all duration-300 justify-center whitespace-nowrap rounded-lg px-3.5 py-2.5 text-sm font-medium text-white shadow-sm shadow-vue/10 focus:outline-none focus:bg-vue focus:ring focus:ring-vue/40 focus-visible:outline-none focus-visible:ring focus-visible:ring-vue/40">Verify Account</button>
-                    </div>
-                    <div class="text-sm text-slate-400 mt-4">Didn't receive code?
-                        <span v-if="!timer" class="font-medium text-vue hover:text-laravel/80 cursor-pointer" @click="handleOtpRequest">Resend</span>
-                        <span v-else class="font-medium text-vue">Resend in {{ timer_html }}</span>
-                    </div>
-                </form>
                 <transition name="fade" mode="out-in">
                     <form v-if="!isOtpSent" @submit.prevent="handleOtpRequest()" class="w-full max-w-xl mx-auto text-center bg-gray-100 dark:bg-gray-900 px-8 py-10 rounded-xl transition-all duration-300 shadow-md hover:shadow-vue dark:shadow-none">
                         <header class="mb-8">
@@ -197,20 +140,32 @@ onMounted(() => {
                     <form v-else id="otp-form" @submit.prevent="handleOtpVerification()" class="w-full max-w-xl mx-auto text-center bg-gray-100 dark:bg-gray-900 px-8 py-10 rounded-xl transition-all duration-300 shadow-md hover:shadow-vue dark:shadow-none">
                         <header class="mb-8">
                             <h1 class="text-2xl font-bold mb-3 text-slate-600 dark:text-white">Email Verification</h1>
-                            <p class="text-[15px] text-slate-400">Enter the 4-digit verification code that was sent to <br>
+                            <p class="text-[15px] text-slate-400">Enter the 6-digit verification code that was sent to <br>
                                 <span class="flex gap-2 items-center justify-center mt-1 hover:text-slate-800 dark:hover:text-white cursor-pointer" @click="edit_email">
                                     <b>"{{ loginData.email }}"</b>
                                     <i class="fas fa-pencil-alt"></i>
                                 </span>
                             </p>
                         </header>
-                        <div class="flex items-center justify-center gap-3">
-                            <input type="password" v-model="loginData.otp[0]" class="w-12 h-12 text-center text-2xl font-extrabold dark:text-gray-100 dark:bg-gray-700 border border-transparent dark:hover:border-slate-200 appearance-none rounded p-4 outline-none dark:focus:bg-gray-600 focus:border-vue/90 focus:ring-2 focus:ring-vue/40" pattern="\d*" maxlength="1" />
-                            <input type="password" v-model="loginData.otp[1]" class="w-12 h-12 text-center text-2xl font-extrabold dark:text-gray-100 dark:bg-gray-700 border border-transparent dark:hover:border-slate-200 appearance-none rounded p-4 outline-none dark:focus:bg-gray-600 focus:border-vue/90 focus:ring-2 focus:ring-vue/40" pattern="\d*" maxlength="1" />
-                            <input type="password" v-model="loginData.otp[2]" class="w-12 h-12 text-center text-2xl font-extrabold dark:text-gray-100 dark:bg-gray-700 border border-transparent dark:hover:border-slate-200 appearance-none rounded p-4 outline-none dark:focus:bg-gray-600 focus:border-vue/90 focus:ring-2 focus:ring-vue/40" pattern="\d*" maxlength="1" />
-                            <input type="password" v-model="loginData.otp[3]" class="w-12 h-12 text-center text-2xl font-extrabold dark:text-gray-100 dark:bg-gray-700 border border-transparent dark:hover:border-slate-200 appearance-none rounded p-4 outline-none dark:focus:bg-gray-600 focus:border-vue/90 focus:ring-2 focus:ring-vue/40" pattern="\d*" maxlength="1" />
-                            <input type="password" v-model="loginData.otp[4]" class="w-12 h-12 text-center text-2xl font-extrabold dark:text-gray-100 dark:bg-gray-700 border border-transparent dark:hover:border-slate-200 appearance-none rounded p-4 outline-none dark:focus:bg-gray-600 focus:border-vue/90 focus:ring-2 focus:ring-vue/40" pattern="\d*" maxlength="1" />
-                            <input type="password" v-model="loginData.otp[5]" class="w-12 h-12 text-center text-2xl font-extrabold dark:text-gray-100 dark:bg-gray-700 border border-transparent dark:hover:border-slate-200 appearance-none rounded p-4 outline-none dark:focus:bg-gray-600 focus:border-vue/90 focus:ring-2 focus:ring-vue/40" pattern="\d*" maxlength="1" />
+                        <div class="flex items-center justify-center">
+                            <PinInput
+                                id="pin-input"
+                                v-model="loginData.otp"
+                                placeholder="○"
+                                @complete="handleComplete"
+                            >
+                                <PinInputGroup class="gap-1">
+                                    <template v-for="(id, index) in 6" :key="id">
+                                        <PinInputSlot
+                                            class="rounded-md border w-12 h-12 text-center text-2xl font-extrabold text-gray-100 bg-gray-700 border-transparent hover:border-slate-200 focus:bg-gray-500 focus:border-vue/90 focus:ring-2 focus:ring-vue/40"
+                                            :index="index"
+                                        />
+                                        <template v-if="index !== 5">
+                                            <PinInputSeparator />
+                                        </template>
+                                    </template>
+                                </PinInputGroup>
+                            </PinInput>
                         </div>
                         <div v-if="errorMessage" class="text-laravel/80 text-sm mt-3">
                             {{ errorMessage }}
